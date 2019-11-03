24a80356a0109e8de0a8cef1f5e30a27272efd2b
Hamza <muhammadhamza1311@gmail.com>
2019-11-03 21:24:51 +0500

    moved quotes=and-couters to autorespond

--- cogs/autorespond.py
+++ cogs/autorespond.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from keys import EPIC_EMOJIS_LINK
+from keys import EPIC_EMOJIS_LINK, QUOTES_CHANNELS, SET_QUOTES_CHANNEL, COUNTERS_FOR_SERVER, UPDATE_COUNTER, COUNTERS
 import discord, re, random, requests, asyncio
 
 
@@ -18,7 +18,7 @@ your little "clever" comment was about to bring down upon you, maybe you would h
 couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will 
 drown in it. You're fucking dead, kiddo. """
 
-class Autoresponder(commands.Cog):
+class AutoresponderCounterQuotes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         req = requests.get(EPIC_EMOJIS_LINK)
@@ -26,6 +26,20 @@ class Autoresponder(commands.Cog):
         self.epic_emojis = []
         for i in data:
             self.epic_emojis.append(i['command'])
+
+        self.quotes_channels = requests.get(QUOTES_CHANNELS).json()
+        self.starred_messages = []
+
+        print(self.quotes_channels)
+
+        data = requests.get(COUNTERS).json()
+        print(data)
+        self.counters = {}
+
+        for i in data:
+            self.counters[i['name']] = i['count']
+        
+        print(self.counters)
 
 
     @commands.command()
@@ -79,7 +93,88 @@ class Autoresponder(commands.Cog):
             elif re.fullmatch(r"\bsmh\b", msg):
                 await cnl.send(random.choice(["Shaking my smh", "Smh my head", "Ikr", "Shaking my head"]))
 
+        for counter in self.counters.keys():
+            rabbitMatch = r"(kaylie'?s? ?(man)|rabbit(man)?)"
+            if (str(counter).lower() == 'rabbit') and re.search(rabbitMatch, msg, re.IGNORECASE):
+                self.counters[counter] += 1
+                requests.post(UPDATE_COUNTER, json={
+                    'serverId': str(cnl.guild.id),
+                    'counter': counter,
+                    'value': self.counters[counter],
+                })
+
+                channel = cnl.guild.get_channel(int(self.quotes_channels[str(cnl.guild.id)]))
+                await channel.send(f"Someone said rabbit <:rabbitman:593375171880943636>, the Kaylie's man.\n{counter} count: {self.counters[counter]}")
+                return
+
+
+            if str(counter).lower() in msg:
+                self.counters[counter] += 1
+                requests.post(UPDATE_COUNTER, json={
+                    'serverId': str(cnl.guild.id),
+                    'counter': counter,
+                    'value': self.counters[counter],
+                })
+
+                channel = cnl.guild.get_channel(int(self.quotes_channels[str(cnl.guild.id)]))
+                await channel.send(f'Someone said {counter}.\n{counter} count: {self.counters[counter]}')
+    
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        print('str(reaction)')
+        print(str(reaction))
+
+        if str(reaction) == "📌":
+            await reaction.message.pin()
+            await reaction.message.remove_reaction(reaction, user)
+            await reaction.message.channel.send(f"{user.mention} Pinned a message!")
+
+        elif str(reaction) == "⭐":
+
+           
+            if reaction.count < 2:
+                print('nope')
+                return
+            if reaction.message.id in self.starred_messages:
+                print('nah')
+                return
+            
+            guild = reaction.message.channel.guild
+            cnl = guild.get_channel(int(self.quotes_channels[str(guild.id)]))
+            url = reaction.message.jump_url
+            
+            embed = discord.Embed(title = 'Go to message',url = url)
+            embed.set_footer(text=f"- {reaction.message.author.name}")
+            embed.colour = discord.Color(value=random.randint(0, 16777215))
+
+            content = reaction.message.content
+            if content:
+                embed.description = content
+
+            attachments = reaction.message.attachments
+            if attachments:
+                embed.set_image(url=attachments[0].url)
+
+            await cnl.send(embed = embed)
+            self.starred_messages.append(reaction.message.id)
+    
+    @commands.is_owner()
+    @commands.command()
+    async def setQuotesChannel(self, ctx, channel: discord.TextChannel):
+        req = requests.post(SET_QUOTES_CHANNEL, {
+            'guildId': str(ctx.message.guild.id),
+            'channelId': str(channel.id),
+        })
+
+        if req.status_code != 200:
+            await ctx.send('An error occured while saving')
+            return
+        
+        self.quotes_channels = requests.get(QUOTES_CHANNELS).json()
+        
+        await ctx.send('Saved')
 
 def setup(bot):
-    bot.add_cog(Autoresponder(bot))
+    bot.add_cog(AutoresponderCounterQuotes(bot))
 
