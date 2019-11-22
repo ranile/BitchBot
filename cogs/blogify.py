bfc9472258839bdeb9df529a8ad6c5ef124e9f3a
hamza1311 <muhammadhamza1311@gmail.com>
2019-11-22 20:41:16 +0500

    More blogify

--- cogs/blogify.py
+++ cogs/blogify.py
@@ -18,15 +18,7 @@ class Blogify(commands.Cog):
     async def blogify(self, ctx):
         """Commands for blogify
         """
-
         pass
-    
-    @blogify.group(invoke_without_command=False)
-    async def users(self, ctx):
-        """Users group
-        """
-
-        await ctx.send('users')
 
     @blogify.group(invoke_without_command=True)
     async def articles(self, ctx):
@@ -61,7 +53,46 @@ class Blogify(commands.Cog):
         embed.description = article['content'] if len(article['content']) < 2000 else (article['content'][:1988] + '**snippet**')
         
         await ctx.send(embed=embed)
+
+    @articles.group()
+    async def search(self, ctx, query):
+        """
+        """
+
+        articles = requests.get(f'{BLOGIFY_API_URL}/articles/search?q={query}&fields=title,summary').json()
+
+        embed = discord.Embed(title="Articles", color = random_discord_color(), url = BLOGIFY_URL)
+        embed.set_footer(text = 'From Blogify')
+
+        for article in articles:
+            embed.add_field(name=f'**{article["title"]}**', value = article['summary'], inline=False)
         
+        await ctx.send(embed=embed)
+
+    @blogify.group(invoke_without_command=False)
+    async def users(self, ctx):
+        """Users group
+        """
+        pass
+
+    @users.group()
+    async def profile(self, ctx, username):
+        """
+        """
+
+        user = requests.get(f'{BLOGIFY_API_URL}/users/byUsername/{username}/?fields=username,profilePicture,name').json()
+
+        embed = discord.Embed(color = random_discord_color())
+        embed.add_field(name='**Username**', value=user['username'], inline=False)
+        embed.add_field(name='**Name**', value=user['name'], inline=False)
+        embed.add_field(name='**Profile**', value=f'[Go to profile]({BLOGIFY_URL}/profile/{user["username"]})', inline=False)
+        embed.set_thumbnail(url = f'{BLOGIFY_API_URL}/get/{user["profilePicture"]["fileId"]}')
+
+        embed.set_footer(text = 'From Blogify')
+        
+        await ctx.send(embed=embed)
+
+
 
 def setup(bot):
     bot.add_cog(Blogify(bot))
