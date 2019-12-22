4cdb5cfdef1ca615d7111ed97df9d10db5a92bf5
Hamza <muhammadhamza1311@gmail.com>
2019-12-22 19:22:47 +0500

    Use aiohttp, not requests + use pagination for reddit and urban

--- cogs/internet.py
+++ cogs/internet.py
@@ -1,17 +1,24 @@
+import discord
+import re
+import urllib
+import wikipedia
 from discord.ext import commands
-import discord, wikipedia, requests, re, random, urllib
-from util.funs import random_discord_color # pylint: disable=no-name-in-module
+
+from util.funs import random_discord_color  # pylint: disable=no-name-in-module
+from util.paginator import Paginator
+
 
 class Internet(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.is_image_regex = re.compile(r".*\.(jpg|png|gif)$")
 
     @commands.command(aliases=["wiki"])
     async def wikipedia(self, ctx, *, search):
         """
         Find a Wikipedia page on a given topic
         """
-        
+
         await ctx.channel.trigger_typing()
         search = wikipedia.search(search)
 
@@ -25,28 +32,7 @@ class Internet(commands.Cog):
         image = page.images[0]
         url = page.url
 
-        embed = discord.Embed(title=title, description=body, url=url, color = random_discord_color())
-        embed.set_footer(text="From Wikipedia")
-        embed.set_thumbnail(url=image)
-
-        await ctx.send(embed=embed)
-
-    @commands.command(aliases=["randomwiki", "wikirand"])
-    async def wikirandom(self, ctx):
-        """
-        Random Wikipedia page
-        """
-
-        await ctx.channel.trigger_typing()
-        search = wikipedia.search(wikipedia.random())
-
-        page = wikipedia.page(search[0])
-        title = page.title
-        body = page.summary[:1000]
-        image = page.images[0]
-        url = page.url
-
-        embed = discord.Embed(title=title, description=body, url=url, color = random_discord_color())
+        embed = discord.Embed(title=title, description=body, url=url, color=random_discord_color())
         embed.set_footer(text="From Wikipedia")
         embed.set_thumbnail(url=image)
 
@@ -59,10 +45,10 @@ class Internet(commands.Cog):
         """
 
         await ctx.channel.trigger_typing()
-        req = requests.get("https://icanhazdadjoke.com", headers={"Accept": "text/plain"})
-        content = req.content.decode("UTF-8")
+        async with self.bot.clientSession.get("https://icanhazdadjoke.com", headers={"Accept": "text/plain"}) as res:
+            content = await res.json(content_type=None)
 
-        await ctx.send(content)
+            await ctx.send(content)
 
     @commands.command()
     async def norris(self, ctx):
@@ -71,50 +57,45 @@ class Internet(commands.Cog):
         """
 
         await ctx.channel.trigger_typing()
-        req = requests.get("https://api.chucknorris.io/jokes/random").json()["value"]
-        await ctx.send(req)
+        async with self.bot.clientSession.get("https://api.chucknorris.io/jokes/random") as res:
+            data = (await res.json())["value"]
+            await ctx.send(data)
 
     @commands.command()
     async def reddit(self, ctx, *, search):
         """
         Get a random reddit post
         """
-        
+
         await ctx.channel.trigger_typing()
-        req = requests.get(f'http://reddit.com/r/{search}/new/.json', headers={'User-agent': 'Chrome'})
-        json = req.json()
+        async with self.bot.clientSession.get(f'http://reddit.com/r/{search}/new/.json', headers={'User-agent': 'Chrome'}) as res:
+            json = await res.json()
 
-        if "error" in json or json["data"]["after"] is None:
-            await ctx.send(f"Subreddit '{search}' not found")
-            return
+            if "error" in json or json["data"]["after"] is None:
+                await ctx.send(f"Subreddit '{search}' not found")
+                return
 
-        req_len = len(json["data"]["children"])
-        rand = random.randint(0, req_len-1)
-        post = json["data"]["children"][rand]
+            posts = json["data"]["children"]
+            embeds = []
+            for post in posts:
+                post = post['data']
 
-        title = post["data"]["title"]
-        author = "u/" + post["data"]["author"]
-        subreddit = post["data"]['subreddit_name_prefixed']
-        url = post["data"]["url"]  # can be image or post link
-        link = "https://reddit.com" + post["data"]["permalink"]
+                if post['over_18'] and not ctx.channel.is_nsfw():
+                    continue
 
-        if "selftext" in post["data"]:
-            text = post["data"]["selftext"]  # may not exist
+                embed = discord.Embed(title=post["title"], url=f'https://reddit.com{post["permalink"]}',
+                                      color=random_discord_color())
+                embed.set_author(name=f'u/{post["author"]}')
+                embed.set_footer(text=post['subreddit_name_prefixed'])
 
-            if len(text) >= 2000:
-                text = text[:2000].rsplit(" ", 1)[0] + " **-Snippet-**"
-            embed = discord.Embed(title=title, description=text, url=link)
+                if post['is_self']:
+                    embed.description = post['selftext'][:800]
+                elif re.match(self.is_image_regex, post['url']):
+                    embed.set_image(url=post['url'])
 
-        else:
-            embed = discord.Embed(title=title, url=link)
+                embeds.append(embed)
 
-        if re.match(r".*\.(jpg|png|gif)$", url):
-            embed.set_image(url=url)
-
-        embed.colour = random_discord_color()
-        embed.set_footer(text=f"From new in {subreddit} by {author}")
-
-        await ctx.send(embed=embed)
+        await Paginator(ctx, embeds).paginate()
 
     @commands.command()
     async def fact(self, ctx):
@@ -123,8 +104,8 @@ class Internet(commands.Cog):
         """
 
         await ctx.channel.trigger_typing()
-        text = requests.get("http://randomuselessfact.appspot.com/random.json?language=en").json()["text"]
-        await ctx.send(text)
+        async with self.bot.clientSession.get("http://randomuselessfact.appspot.com/random.json?language=en") as res:
+            await ctx.send(((await res.json())['text']))
 
     @commands.command(aliases=["belikebill"])
     async def bill(self, ctx, *, name="Bill"):
@@ -136,26 +117,31 @@ class Internet(commands.Cog):
         await ctx.send(embed=discord.Embed().set_image(url=link))
 
     @commands.command()
-    async def urban(self, ctx, query, index = 0):
+    async def urban(self, ctx, *, query):
         """
-        Gets top defination from urban dictionary
+        Gets top definition from urban dictionary
         """
-        
+
         await ctx.channel.trigger_typing()
         search = urllib.parse.quote(query)
-        url = f"http://api.urbandictionary.com/v0/define?term={search}"
         link = f"https://www.urbandictionary.com/define.php?term={search}"
-        req = requests.get(url)
 
-        if req.status_code == 404:
-            await ctx.send('Errorrrrr... Not found')
-        data = req.json()
+        async with self.bot.clientSession.get(f"http://api.urbandictionary.com/v0/define?term={search}") as res:
+            if res.status == 404:
+                await ctx.send('Errorrrrr... Not found')
+                return
+            data = (await res.json())['list']
+            embeds = []
+            for item in data:
+                embed = discord.Embed(title=item['word'], description=item['definition'], url=link,
+                                      color=random_discord_color())
 
-        item = data['list'][index]
-        embed = discord.Embed(title = query, description=  item['definition'], url=link, color = random_discord_color())
-        embed.add_field(name = "Example", value = item["example"], inline=False)
-        embed.set_footer(text = "From Urban Dictionary")
-        await ctx.send(embed = embed)
+                embed.add_field(name="Example", value=item["example"], inline=False)
+                embed.set_footer(text="From Urban Dictionary")
+
+                embeds.append(embed)
+
+            await Paginator(ctx, embeds).paginate()
 
     @commands.command(aliases=["insult"])
     async def roastme(self, ctx):
@@ -164,13 +150,15 @@ class Internet(commands.Cog):
         """
 
         await ctx.channel.trigger_typing()
-        url = "https://insult.mattbas.org/api/insult.json"
-        req = requests.get(url)
-        if req.status_code != 200:
-            await ctx.send("You lucky bastard... An error occuered\nMission failed bois, we'll get 'em next time")
-            return
-        
-        await ctx.send(req.json()['insult'])
+
+        async with self.bot.clientSession.get("https://insult.mattbas.org/api/insult.json") as res:
+            if res.status != 200:
+                await ctx.send("You lucky bastard... An error occurred."
+                               "Mission failed bois, we'll get 'em next time")
+                return
+
+            await ctx.send((await res.json())['insult'])
+
 
 def setup(bot):
     bot.add_cog(Internet(bot))
