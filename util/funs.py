204d112227830365f7e442e3a640f44c8e00fd5e
Hamza <muhammadhamza1311@gmail.com>
2019-12-22 19:42:53 +0500

    misc improvements

--- util/funs.py
+++ util/funs.py
@@ -3,11 +3,8 @@ from discord.ext import commands
 from random import randint
 
 from keys import logWebhook, rabbitWebhook
-import aiohttp
 import inspect
 import re
-import asyncio
-import datetime
 
 
 def random_discord_color():
@@ -16,10 +13,7 @@ def random_discord_color():
 
 def cause_check():
     def predicate(ctx):
-        if isinstance(ctx, discord.Guild):
-            guild = ctx
-        else:
-            guild = ctx.guild
+        guild = ctx.guild
 
         if guild.id == 505655510263922700:
             return True
@@ -29,19 +23,20 @@ def cause_check():
     return commands.check(predicate)
 
 
+# noinspection PyPep8Naming
 def getInfoFromDocstring(docstring):
-    """Gets information from docstring formatted using Google's python styleguide.
+    """Gets information from docstring formatted using Google's python style guide.
 
     Args:
-        docstring: The doctring to extract information from.
+        docstring: The docstring to extract information from.
 
     Returns:
-        Tuple of dict of the aruguments and their docs and everything in the docstring before the word `Args: `.
+        Tuple of dict of the arguments and their docs and everything in the docstring before the word `Args: `.
 
     """
 
-    splitted = docstring.split("Args:\n")
-    args = inspect.cleandoc(splitted[1]).split('\n')
+    split = docstring.split("Args:\n")
+    args = inspect.cleandoc(split[1]).split('\n')
 
     docs = {}
     for arg in args:
@@ -55,9 +50,10 @@ def getInfoFromDocstring(docstring):
 
         docs[argName] = argDoc
 
-    return docs, splitted[0][:-2]
+    return docs, split[0][:-2]
 
 
+# noinspection PyPep8Naming
 def generateArgStringForEmbed(args):
     out = ''
     keys = list(args.keys())
@@ -68,6 +64,7 @@ def generateArgStringForEmbed(args):
     return out
 
 
+# noinspection PyPep8Naming
 async def canRunCommand(ctx, command):
     try:
         canRun = await command.can_run(ctx)
@@ -77,7 +74,8 @@ async def canRunCommand(ctx, command):
     return canRun
 
 
-async def log(ctx, username, msg, sentMessage, out=None):
+# noinspection PyPep8Naming
+async def log(ctx, msg, sentMessage, out=None):
     embed = discord.Embed(color=random_discord_color(), description=msg)
     embed.set_author(name=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
     embed.timestamp = ctx.message.created_at
@@ -87,33 +85,19 @@ async def log(ctx, username, msg, sentMessage, out=None):
 
     embed.add_field(name='Message', value=f'[Jump To Message]({sentMessage.jump_url})')
 
-    async with aiohttp.ClientSession() as session:
-        webhook = discord.Webhook.from_url(logWebhook, adapter=discord.AsyncWebhookAdapter(session))
-        await webhook.send(embed=embed, username=username)
+    webhook = discord.Webhook.from_url(logWebhook, adapter=discord.AsyncWebhookAdapter(ctx.bot.clientSession))
+    await webhook.send(embed=embed, username=ctx.command.name)
 
 
-async def sendRabbitCounterUpdate(msg):
+# noinspection PyPep8Naming
+async def sendRabbitCounterUpdate(bot, msg):
     pfp = 'https://raw.githubusercontent.com/hamza1311/BitchBot/master/res/rabbitman2.jpg'
 
-    async with aiohttp.ClientSession() as session:
-        webhook = discord.Webhook.from_url(rabbitWebhook, adapter=discord.AsyncWebhookAdapter(session))
-        await webhook.send(msg, username='Rabbit', avatar_url=pfp)
+    webhook = discord.Webhook.from_url(rabbitWebhook, adapter=discord.AsyncWebhookAdapter(bot.clientSession))
+    await webhook.send(msg, username='Rabbit', avatar_url=pfp)
 
 
-async def motivate():
-    while True:
-        print('called')
-        timeTillFreedom = datetime.datetime(year=2019, month=12, day=20, hour=12) - datetime.datetime.now()
-
-        async with aiohttp.ClientSession() as session:
-            webhook = discord.Webhook.from_url(moritvatorWebhook, adapter=discord.AsyncWebhookAdapter(session))
-            await webhook.send(
-                f'Hey <@529535587728752644>, you got {timeTillFreedom.days} days till freedom.\nPull through!!')
-            await asyncio.sleep((24 - (datetime.datetime.utcnow().hour + 5)) * 60 * 60)
-            await webhook.send(
-                f'Hey <@529535587728752644>, you got {timeTillFreedom.days} days till freedom.\nPull through!!')
-
-
+# noinspection PyPep8Naming
 def generateDollarSigns(collection):
     dollarSigns = ''
     for i in range(len(collection)):
