d9df90cc0a7bef355fd9bbfc590527c63faa9e74
Hamza <muhammadhamza1311@gmail.com>
2020-01-09 17:08:50 +0500

    Subclass bot and code up code

---
+++ BitchBot.py
@@ -0,0 +1,68 @@
+import asyncio
+import logging
+
+import aiohttp
+import discord
+import tornado.web
+from discord.ext import commands
+
+from database import database
+# noinspection PyPep8Naming
+from routes.routes import routesList as routes
+from util.HelpCommand import BloodyHelpCommand
+
+
+class BitchBot(commands.Bot):
+    def __init__(self, **kwargs):
+        super().__init__(
+            command_prefix='>',
+            help_command=BloodyHelpCommand(),
+            owner_id=529535587728752644,
+            case_insensitive=True,
+        )
+
+        self.tornado_app = tornado.web.Application([(route, handler, dict(bot=self)) for route, handler in routes], **{
+            'debug': True
+        })
+        self.clientSession = None
+
+    # noinspection PyMethodMayBeStatic,SpellCheckingInspection
+    def setup_discordpy_logger(self):
+        dpy_logger = logging.getLogger('discord')
+        dpy_logger.setLevel(logging.DEBUG)
+        handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+        handler.setFormatter(logging.Formatter('%(name)s: %(levelname)s: %(asctime)s: %(message)s'))
+        dpy_logger.addHandler(handler)
+
+    # noinspection PyAttributeOutsideInit
+    async def setup(self, **kwargs):
+        self.setup_discordpy_logger()
+        self.tornado_app.listen(6969)
+        self.db = await database.init(self.loop)
+        cogs = kwargs.pop('cogs')
+        for cog_name in cogs:
+            self.load_extension(f"cogs.{cog_name}")
+        await self.start(kwargs.pop('token'))
+
+    async def stop(self, _):
+        await self.logout()
+        await self.loop.stop()
+
+    def run(self, token, cogs):
+        future = asyncio.ensure_future(self.setup(token=token, cogs=cogs), loop=self.loop)
+        future.add_done_callback(self.stop)
+        try:
+            self.loop.run_forever()
+        finally:
+            future.remove_done_callback(self.stop)
+
+    async def on_ready(self):
+        print(f"{self.user.name} is running")
+        self.db = await database.init(self.loop)
+        print("-" * len(self.user.name + " is running"))
+        await self.change_presence(
+            status=discord.Status.online,
+            activity=discord.Game(f"use {self.command_prefix}help")
+        )
+        if self.clientSession is None:
+            self.clientSession = aiohttp.ClientSession()
--- core.py
+++ core.py
@@ -1,38 +1,15 @@
-import asyncio
-
-# noinspection PyPackageRequirements
-import discord
-import tornado.web
-# noinspection PyPackageRequirements
 from discord.ext import commands
-
-from database import database
-from keys import bot as BOT_TOKEN
-from routes.routes import routesList as routes
-from util import HelpCommand
-# noinspection PyPackageRequirements
-import aiohttp
-import logging
-
-bot = commands.Bot(
-    command_prefix=">",
-    case_insensitive=True,
-    owner_id=529535587728752644,
-    help_command=HelpCommand.BloodyHelpCommand()
-)
-
-dpy_logger = logging.getLogger('discord')
-dpy_logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(name)s: %(levelname)s: %(asctime)s: %(message)s'))
-dpy_logger.addHandler(handler)
+import keys
+from BitchBot import BitchBot
 
 cogs = ["admin", "cause", "emojis", "internet", "starboard", 'activity', 'jsk', 'logs', 'mod', 'misc', 'anime']
+
+bot = BitchBot(cogs=cogs)
 
 
 @bot.command()
 @commands.is_owner()
-async def reload(ctx: commands.Context, module: str):
+async def reload(ctx, module):
     """
     Reloads a cog
 
@@ -43,47 +20,4 @@ async def reload(ctx: commands.Context, module: str):
     bot.reload_extension(f'cogs.{module}')
     await ctx.send("🔄")
 
-
-@bot.event
-async def on_ready():
-    print(f"{bot.user.name} is running")
-    print("-" * len(bot.user.name + " is running"))
-    await bot.change_presence(
-        status=discord.Status('online'),
-        activity=discord.Game(f"use {bot.command_prefix}help")
-    )
-
-    bot.clientSession = aiohttp.ClientSession()
-    for i in cogs:
-        bot.load_extension(f"cogs.{i}")
-
-
-async def close():
-    await bot.logout()
-
-
-async def start():
-    try:
-        pool = await database.init(loop)
-        bot.db = pool
-        await bot.start(BOT_TOKEN)
-    finally:
-        await bot.close()
-
-
-def stop_loop_on_completion(_):
-    loop.stop()
-
-
-app = tornado.web.Application([(route, handler, dict(bot=bot)) for route, handler in routes], **{
-    'debug': True
-})
-# app.listen(6969)
-loop = asyncio.get_event_loop()
-
-future = asyncio.ensure_future(start(), loop=loop)
-future.add_done_callback(stop_loop_on_completion)
-try:
-    loop.run_forever()
-finally:
-    future.remove_done_callback(stop_loop_on_completion)
+bot.run(keys.bot, cogs=cogs)
