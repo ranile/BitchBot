d8125a547160e718c1d4a99c66369778d26560ce
Hamza <muhammadhamza1311@gmail.com>
2020-01-18 18:07:27 +0500

    do not use docker cache while building

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ fi
 
 docker stop bitch_bot
 docker rm bitch_bot
-docker build -t bitch_bot .
+docker build --no-cache -t bitch_bot .
 docker run -d --network=host --name=bitch_bot bitch_bot
 echo Sucessfully deployed
