7faf8b3df6d96ea8d0f0c7c7fae948a95caeadef
Hamza <muhammadhamza1311@gmail.com>
2020-01-18 13:11:52 +0500

    cleanup core.py

--- core.py
+++ core.py
@@ -1,24 +1,8 @@
-from discord.ext import commands
 import keys
 from BitchBot import BitchBot
 
 cogs = ["admin", "cause", "emojis", "internet", "starboard", 'activity', 'jsk', 'logs', 'mod', 'misc']
 
 bot = BitchBot(cogs=cogs)
-
-
-
-@commands.is_owner()
-async def reload(ctx, module):
-    """
-    Reloads a cog
-
-    Args:
-        module: The cog to reload
-    """
-
-    bot.reload_extension(f'cogs.{module}')
-    await ctx.send("🔄")
-
 
 bot.run(keys.bot)
