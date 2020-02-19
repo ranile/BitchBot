0ce1227f1b682257916e375df3f900d869a1c1c3
Hamza <muhammadhamza1311@gmail.com>
2020-02-19 20:35:57 +0500

    add show-warns component

---
+++ web/frontend/src/app/components/show-warns/show-warns.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-warns',
+  templateUrl: './show-warns.component.html',
+  styleUrls: ['./show-warns.component.scss']
+})
+export class ShowWarnsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
