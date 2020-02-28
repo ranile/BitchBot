1d6033aa80e0ab54480ebab1d5b3908972762db8
Hamza <muhammadhamza1311@gmail.com>
2020-02-28 22:24:20 +0500

    set avatar as favicon

--- web/frontend/src/app/app.component.ts
+++ web/frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,14 @@ export class AppComponent implements OnInit  {
   title = 'bitch-bot';
   avatarUrl: string = '';
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, @Inject(DOCUMENT) private _document: HTMLDocument) {
   }
 
   ngOnInit(): void {
     this.httpClient.get('/api/icon').toPromise().then(resp => {
-      this.avatarUrl = resp['url']
+      const url = resp['url']
+      this.avatarUrl = url
+      this._document.getElementById('appFavicon').setAttribute('href', url);
     })
   }
 
