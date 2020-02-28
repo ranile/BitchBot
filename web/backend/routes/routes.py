90b53d15b08fa8a4b1cb7e8f4c487b966899acfa
Hamza <muhammadhamza1311@gmail.com>
2020-02-28 22:14:03 +0500

    use avatar as icon in frontend

--- web/backend/routes/routes.py
+++ web/backend/routes/routes.py
@@ -1,6 +1,8 @@
+from quart import jsonify
+
 from util import quart_fix
 
-blueprint = quart_fix.BlueprintWithBot('my_blueprint', __name__)
+blueprint = quart_fix.BlueprintWithBot('my_blueprint', __name__, url_prefix='/api')
 
 
 @blueprint.route("/heartbeat")
@@ -9,11 +11,9 @@ def heartbeat():
             'user': str(blueprint.bot.get_user(529535587728752644))}
 
 
-@blueprint.route('/user/<int:user_id>')
-async def user(user_id):
-    usr = blueprint.bot.get_user(int(user_id))
-    print(user_id, usr)
-    return {'name': str(usr)}
+@blueprint.route('/icon')
+async def icon():
+    return jsonify(url=str(blueprint.bot.get_user(595363392886145046).avatar_url_as(size=256)))
 
 
 def setup(bot):
--- web/frontend/src/app/app.component.ts
+++ web/frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'bitch-bot';
+  avatarUrl: string = '';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get('/api/icon').toPromise().then(resp => {
+      this.avatarUrl = resp['url']
+    })
+  }
+
 }
