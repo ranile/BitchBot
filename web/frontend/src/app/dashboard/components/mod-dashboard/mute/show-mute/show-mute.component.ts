bc490a3109cd557e845833d952de063b7175d656
Hamza <muhammadhamza1311@gmail.com>
2020-02-29 22:51:54 +0500

    add mute place holder components

---
+++ web/frontend/src/app/dashboard/components/mod-dashboard/mute/show-mute/show-mute.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-mute',
+  templateUrl: './show-mute.component.html',
+  styleUrls: ['./show-mute.component.scss']
+})
+export class ShowMuteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- web/frontend/src/app/dashboard/dashboard.module.ts
+++ web/frontend/src/app/dashboard/dashboard.module.ts
@@ -10,13 +10,16 @@ import {MatListModule} from "@angular/material/list";
 import { ShowAllRemindersComponent } from './components/reminders/show-all-reminders/show-all-reminders.component';
 import { ShowReminderComponent } from './components/reminders/show-reminder/show-reminder.component';
 import {MatCardModule} from "@angular/material/card";
+import { ShowAllMutesComponent } from './components/mod-dashboard/mute/show-all-mutes/show-all-mutes.component';
+import { ShowMuteComponent } from './components/mod-dashboard/mute/show-mute/show-mute.component';
 
 
 
 @NgModule({
-    declarations: [MainDashboardComponent, ShowAllRemindersComponent, ShowReminderComponent],
+    declarations: [MainDashboardComponent, ShowAllRemindersComponent, ShowReminderComponent, ShowAllMutesComponent, ShowMuteComponent],
     exports: [
-        MainDashboardComponent
+        MainDashboardComponent,
+        ShowAllMutesComponent
     ],
     imports: [
         CommonModule,
