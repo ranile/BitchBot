36cf3fd77655838008bbb14a7f7794289e32d3c0
Hamza <muhammadhamza1311@gmail.com>
2020-03-21 00:08:40 +0500

    start work on home page

--- web/frontend/src/app/components/home/home.component.ts
+++ web/frontend/src/app/components/home/home.component.ts
@@ -1,23 +1,28 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth/auth.service";
 import {Router} from "@angular/router";
-import { DOCUMENT } from '@angular/common';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router, @Inject(DOCUMENT) private document: Document) { }
+    constructor(private authService: AuthService, private router: Router, @Inject(DOCUMENT) private document: Document) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  login() {
-    this.authService.login().then(it => {
-      this.document.location.href = it['url']
-    })
-  }
+    login() {
+        this.authService.login().then(it => {
+            this.document.location.href = it['url']
+        })
+    }
+
+    navigateToOauthPage() {
+        window.location.href = "invite url"
+    }
 }
