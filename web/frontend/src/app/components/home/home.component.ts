3f73eca7c8d129c115b610f1fe03eb1404ace25f
Hamza <muhammadhamza1311@gmail.com>
2020-03-26 21:14:42 +0500

    improve home component

--- web/frontend/src/app/components/home/home.component.ts
+++ web/frontend/src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, HostListener, Inject, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth/auth.service";
 import {Router} from "@angular/router";
 import {DOCUMENT} from '@angular/common';
+import {UserService} from "../../services/user/user.service";
 
 @Component({
     selector: 'app-home',
@@ -9,11 +10,22 @@ import {DOCUMENT} from '@angular/common';
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    avatarUrl: string;
 
-    constructor(private authService: AuthService, private router: Router, @Inject(DOCUMENT) private document: Document) {
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        @Inject(DOCUMENT) private document: Document,
+        private userService: UserService
+    ) {
     }
 
     ngOnInit(): void {
+        this.document.getElementById('icon-container').style.opacity = '0';
+
+        this.userService.fetchMyAvatarUrl(1024).then(it => {
+            this.avatarUrl = it
+        })
     }
 
     login() {
@@ -24,5 +36,25 @@ export class HomeComponent implements OnInit {
 
     navigateToOauthPage() {
         window.location.href = "https://discordapp.com/oauth2/authorize?client_id=595363392886145046&scope=bot&permissions=388160"
+    }
+
+    lastTopValue = 0
+
+    @HostListener('window:scroll', [])
+    onScroll() {
+        const rect = this.document.getElementById('two').getBoundingClientRect().top
+        console.log(window.innerHeight, rect, this.lastTopValue)
+        if (this.lastTopValue >= (window.innerHeight / 1.7)) {
+            this.document.getElementById('icon-container').style.opacity = '0';
+            this.document.getElementById('home-avatar-container').style.opacity = '1';
+        } else {
+            this.document.getElementById('icon-container').style.opacity = '1';
+            this.document.getElementById('home-avatar-container').style.opacity = '0';
+        }
+        this.lastTopValue = rect
+    }
+
+    navigateToGithubRepo() {
+        window.location.href = "https://www.github.com/hamza1311/BitchBot"
     }
 }
