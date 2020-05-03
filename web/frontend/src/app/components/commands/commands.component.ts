14a719e1d1972e4cdcebad13317ba62fb4f00e72
Hamza <muhammadhamza1311@gmail.com>
2020-05-03 23:12:21 +0500

    paginate commands on frontend

--- web/frontend/src/app/components/commands/commands.component.ts
+++ web/frontend/src/app/components/commands/commands.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {MatTableDataSource} from "@angular/material/table";
+import {PageEvent} from "@angular/material/paginator";
 
 export interface Command {
     name: string
@@ -9,9 +10,9 @@ export interface Command {
 }
 
 export interface CogCommands {
-    name:  string
-    description:  string
-    commands:  Command[]
+    name: string
+    description: string
+    commands: Command[]
 }
 
 
@@ -24,6 +25,7 @@ export class CommandsComponent implements OnInit {
     displayedColumns: string[] = ['name', 'help', 'signature'];
     cogs = []
     dataSources: { [key: string]: MatTableDataSource<Command> } = {};
+    cog: CogCommands
 
     constructor(private httpClient: HttpClient) {
     }
@@ -34,10 +36,35 @@ export class CommandsComponent implements OnInit {
                 this.cogs.push(it)
                 this.dataSources[it.name] = new MatTableDataSource(it.commands)
             })
+
+            this.cogs = this.cogs.sort((n1, n2) => {
+                if (n1.name > n2.name) {
+                    return 1
+                }
+
+                if (n1.name < n2.name) {
+                    return -1
+                }
+
+                return 0
+            });
+            this.cog = this.cogs[0]
             // this.dataSources[data[0].name] = new MatTableDataSource(data[0].commands)
-            console.log(data)
+            console.log(this.cogs)
         })
 
     }
 
+    shortHelp(help: string): string {
+        if (help == null) return 'Help for this command is not available, join our support server for help'
+        return help.split('\n')[0]
+    }
+
+    pageChange(event: PageEvent) {
+        this.cog = this.cogs[event.pageIndex]
+    }
+
+    onCommandRowClick(row: Command) {
+        console.log(row)
+    }
 }
