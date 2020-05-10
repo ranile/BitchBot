995fa4db96a5c72d80811507ac5142ea05de9c9c
Hamza <muhammadhamza1311@gmail.com>
2020-05-11 00:08:03 +0500

    UI improvements

--- web/frontend/src/app/app.component.ts
+++ web/frontend/src/app/app.component.ts
@@ -1,25 +1,35 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {DOCUMENT} from "@angular/common";
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { DOCUMENT } from "@angular/common";
+import { Router, RouterEvent } from "@angular/router";
+import {ThemePalette} from "@angular/material/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit  {
-  title = 'bitch-bot';
-  avatarUrl: string = '';
+export class AppComponent implements OnInit {
+    avatarUrl: string = '';
+    navbarColor: ThemePalette = 'primary';
 
-  constructor(private httpClient: HttpClient, @Inject(DOCUMENT) private _document: HTMLDocument) {
-  }
+    constructor(
+        private httpClient: HttpClient,
+        @Inject(DOCUMENT) private _document: HTMLDocument,
+        private router: Router) {}
 
-  ngOnInit(): void {
-    this.httpClient.get('/api/icon').toPromise().then(resp => {
-      const url = resp['url']
-      this.avatarUrl = url
-      this._document.getElementById('appFavicon').setAttribute('href', url);
-    })
-  }
+    ngOnInit(): void {
+        this.httpClient.get('/api/icon').toPromise().then(resp => {
+            const url = resp['url']
+            this.avatarUrl = url
+            this._document.getElementById('appFavicon').setAttribute('href', url);
+        })
+
+        this.router.events.subscribe(value => {
+            if (value instanceof RouterEvent) {
+                this.navbarColor = value.url === '/home'? 'primary' : undefined;
+            }
+        })
+    }
 
 }
