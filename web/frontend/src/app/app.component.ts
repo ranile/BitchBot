652ca2faaa448625873784a3027f81f5fab4694e
Hamza <muhammadhamza1311@gmail.com>
2020-06-01 23:44:56 +0500

    little more cleanup

--- web/frontend/src/app/app.component.ts
+++ web/frontend/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from "@angular/common";
 import { Router, RouterEvent } from "@angular/router";
-import {ThemePalette} from "@angular/material/core";
-import {UserService} from "./services/user/user.service";
+import { ThemePalette } from "@angular/material/core";
+import { UserService } from "./services/user/user.service";
 
 @Component({
     selector: 'app-root',
@@ -16,18 +16,19 @@ export class AppComponent implements OnInit {
     constructor(
         private userService: UserService,
         @Inject(DOCUMENT) private _document: HTMLDocument,
-        private router: Router) {}
+        private router: Router
+    ) {
+        this.router.events.subscribe(value => {
+            if (value instanceof RouterEvent) {
+                this.navbarColor = value.url === '/home'? 'primary' : undefined;
+            }
+        })
+    }
 
     ngOnInit(): void {
         this.userService.fetchMyAvatarUrl(1024).then(url => {
             this.avatarUrl = url
             this._document.getElementById('appFavicon').setAttribute('href', url);
-        })
-
-        this.router.events.subscribe(value => {
-            if (value instanceof RouterEvent) {
-                this.navbarColor = value.url === '/home'? 'primary' : undefined;
-            }
         })
     }
 
--- web/frontend/src/app/components/navbar/navbar.component.ts
+++ web/frontend/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {AuthService} from "../../services/auth/auth.service";
 import {UserService} from "../../services/user/user.service";
 import {DOCUMENT} from "@angular/common";
 import {HomeComponent} from "../home/home.component";
@@ -10,38 +9,10 @@ import {HomeComponent} from "../home/home.component";
     styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-    /*currentUser: User;
-    isLoggedIn: boolean = false;*/
 
-    constructor(private authService: AuthService, private userService: UserService, @Inject(DOCUMENT) private document: Document) {
-    }
+    constructor(private userService: UserService, @Inject(DOCUMENT) private document: Document) {}
 
-    ngOnInit(): void {
-        /*this.userService.fetchCurrentUser()
-            .then(resp => {
-                this.currentUser = resp.user
-                this.isLoggedIn = true
-            })
-            .catch(error => {
-                if (error.status == 401) {
-                    this.isLoggedIn = false
-                }
-                console.error(error)
-            })*/
-    }
-
-    /*login() {
-        this.authService.login().then(it => {
-            this.document.location.href = it['url']
-        })
-    }
-
-    logout() {
-        this.authService.logout().then(() => {
-            this.isLoggedIn = false
-            //  TODO: Navigate to /home
-        })
-    }*/
+    ngOnInit(): void {}
 
     navigateToInvite() {
         this.document.location.href = HomeComponent.INVITE_URL
