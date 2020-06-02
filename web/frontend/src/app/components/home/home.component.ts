f4a96fef7cabcd920d66a34652ae796c06a44f85
Hamza <muhammadhamza1311@gmail.com>
2020-06-02 21:51:32 +0500

    Formatting + fix bot link

--- web/frontend/src/app/components/home/home.component.ts
+++ web/frontend/src/app/components/home/home.component.ts
@@ -12,13 +12,13 @@ import {Stats} from "../../models/Stats";
 export class HomeComponent implements OnInit {
     avatarUrl: string;
     static INVITE_URL = "https://discordapp.com/oauth2/authorize?client_id=595363392886145046&scope=bot&permissions=388160"
+    REPO_URL          = "https://www.github.com/hamza1311/BitchBot"
+    DBOTS_URL         = "https://discord.bots.gg/bots/595363392886145046"
+    DISCORDAPPS_URL   = "https://discordapps.dev/en-GB/bots/595363392886145046"
+    DBL_URL           = "https://top.gg/bot/595363392886145046"
+    LIST_MY_BOTS_URL  = 'https://listmybots.com/bots/595363392886145046'
+    BOTLIST_SPACE_URL = 'https://botlist.space/bot/595363392886145046'
     INVITE_URL = HomeComponent.INVITE_URL // So it can be used in template
-    REPO_URL = "https://www.github.com/hamza1311/BitchBot"
-    DBOTS_URL = "https://discord.bots.gg/bots/595363392886145046"
-    DISCORDAPPS_URL = "https://discordapps.dev/en-GB/bots/595363392886145046"
-    DBL_URL = "https://top.gg/bot/595363392886145046"
-    LIST_MY_BOTS_URL = "https://listmybots.com/bot/595363392886145046"
-    BOTLIST_SPACE_URL = 'https://botlist.space/bot/595363392886145046';
 
     stats: Stats =  {
         uptime: {minutes: 0, seconds: 0, hours: 0, days: 0, human_friendly: '', total_seconds: '0'},
