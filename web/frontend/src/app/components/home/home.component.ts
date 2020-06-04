58af1d48c9d9f073343c56a4fe5e78583c7f030d
Hamza <muhammadhamza1311@gmail.com>
2020-06-04 19:01:16 +0500

    Add bots on discord button + formatting

--- web/frontend/src/app/components/home/home.component.ts
+++ web/frontend/src/app/components/home/home.component.ts
@@ -11,13 +11,14 @@ import {Stats} from "../../models/Stats";
 })
 export class HomeComponent implements OnInit {
     avatarUrl: string;
-    static INVITE_URL = "https://discordapp.com/oauth2/authorize?client_id=595363392886145046&scope=bot&permissions=388160"
-    REPO_URL          = "https://www.github.com/hamza1311/BitchBot"
-    DBOTS_URL         = "https://discord.bots.gg/bots/595363392886145046"
-    DISCORDAPPS_URL   = "https://discordapps.dev/en-GB/bots/595363392886145046"
-    DBL_URL           = "https://top.gg/bot/595363392886145046"
+    static INVITE_URL = 'https://discordapp.com/oauth2/authorize?client_id=595363392886145046&scope=bot&permissions=388160'
+    REPO_URL          = 'https://www.github.com/hamza1311/BitchBot'
+    DBOTS_URL         = 'https://discord.bots.gg/bots/595363392886145046'
+    DISCORDAPPS_URL   = 'https://discordapps.dev/en-GB/bots/595363392886145046'
+    DBL_URL           = 'https://top.gg/bot/595363392886145046'
     LIST_MY_BOTS_URL  = 'https://listmybots.com/bots/595363392886145046'
     BOTLIST_SPACE_URL = 'https://botlist.space/bot/595363392886145046'
+    BOD_URL           = 'https://bots.ondiscord.xyz/bots/595363392886145046'
     INVITE_URL = HomeComponent.INVITE_URL // So it can be used in template
 
     stats: Stats =  {
