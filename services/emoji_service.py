893a2838ba38b6f0a08f9b0b8dfc30de10dbd15d
Hamza <muhammadhamza1311@gmail.com>
2020-06-10 21:03:51 +0500

    emojis: make emoji service work with ctx.db, use new subclass in Cog

--- services/emoji_service.py
+++ services/emoji_service.py
@@ -1,35 +1,34 @@
-from database.sql import SQL
+import asyncpg
+import typing
 
 
 class EmojiService:
-    def __init__(self, pool):
-        self.pool = pool
 
-    async def mark_safe(self, emoji_id, marked_by):
-        async with self.pool.acquire() as connection:
-            fetched = await connection.fetchrow('''
+    @staticmethod
+    async def mark_safe(db: asyncpg.Connection, emoji_id: int, marked_by: int) -> typing.Dict[str, str]:
+        fetched = await db.fetchrow('''
             insert into emojis (emoji_id, marked_by)
             values ($1, $2)
             returning *;
-            ''', emoji_id, marked_by)
-            return fetched
+        ''', emoji_id, marked_by)
 
-    async def fetch_all_safe_emojis(self):
-        async with self.pool.acquire() as connection:
-            fetched = await connection.fetch('''
+        return {  # doing this only so i can return `dict` and have it be accurate
+            'emoji_id': fetched['emoji_id'],
+            'marked_by': fetched['marked_by'],
+        }
+
+    @staticmethod
+    async def fetch_all_safe_emojis(db: asyncpg.Connection) -> typing.List[int]:
+        fetched = await db.fetch('''
             select emoji_id from emojis
-            ''')
-            return [x['emoji_id'] for x in fetched] if fetched is not None else []
+        ''')
+        return [x['emoji_id'] for x in fetched] if fetched is not None else []
 
-    @classmethod
-    def sql(cls):
-        return SQL(
-            createTable='''
-                create table if not exists emojis
-                (
-                    emoji_id        bigint,
-                    marked_by bigint
-                );
-                create unique index if not exists unique_emoji_index on emojis (emoji_id, marked_by);
-            '''
-        )
+    initial_sql = '''
+        create table if not exists emojis
+        (
+            emoji_id        bigint,
+            marked_by bigint
+        );
+        create unique index if not exists unique_emoji_index on emojis (emoji_id, marked_by);
+    '''
