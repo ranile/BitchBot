ee10add9bc52a116b142ae6aac3a257d30190019
Hamza <muhammadhamza1311@gmail.com>
2020-06-10 23:06:29 +0500

    misc: fix quart blueprint subclass

--- web/backend/utils/quart_with_bot.py
+++ web/backend/utils/quart_with_bot.py
@@ -9,4 +9,4 @@ class QuartWithBot(quart.Quart):
 
     def register_blueprint_with_bot(self, blueprint: BlueprintWithBot, bot, url_prefix: Optional[str] = None) -> None:
         blueprint.bot = bot
-        super().register_blueprint(blueprint, url_prefix)
+        super().register_blueprint(blueprint, url_prefix=url_prefix)
