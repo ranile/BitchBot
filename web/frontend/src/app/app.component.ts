dbc4e159fd961525cc8f7d5a9623e162a84be0b7
Hamza <muhammadhamza1311@gmail.com>
2020-06-21 00:29:48 +0500

    frontend: cleanup

--- web/frontend/src/app/app.component.ts
+++ web/frontend/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { ThemePalette } from "@angular/material/core";
 import { DOCUMENT } from "@angular/common";
+import { HomeComponent } from "./components/home/home.component";
+import {Router, RouterEvent} from "@angular/router";
 
 @Component({
     selector: 'app-root',
@@ -11,13 +13,23 @@ export class AppComponent implements OnInit {
     navbarColor: ThemePalette = 'primary';
     hideToolbar = false;
 
-    constructor(@Inject(DOCUMENT) private document: Document) {
-        const location = this.document.location
-        this.hideToolbar = location.href.toLowerCase().includes('iframe')
-        this.navbarColor = location.pathname && location.pathname != '/' ? undefined : 'primary';
+    constructor(
+        @Inject(DOCUMENT) private document: Document,
+        private router: Router
+    ) {
+        this.hideToolbar = this.document.location.href.toLowerCase().includes('iframe')
+
+        this.router.events.subscribe(value => {
+            if (value instanceof RouterEvent) {
+                this.navbarColor = value.url && value.url != '/' ? undefined : 'primary';
+            }
+        })
     }
 
     ngOnInit(): void {
     }
 
+    navigateToInvite() {
+        this.document.location.href = HomeComponent.INVITE_URL
+    }
 }
--- web/frontend/src/app/app.module.ts
+++ web/frontend/src/app/app.module.ts
@@ -1,35 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import {HttpClientModule} from "@angular/common/http";
-import { NavbarComponent } from './components/navbar/navbar.component';
-import {FormsModule} from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from "@angular/material/button";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatListModule} from "@angular/material/list";
-import {MatTabsModule} from "@angular/material/tabs";
-import {MatOptionModule} from "@angular/material/core";
-import {MatSelectModule} from "@angular/material/select";
-import {MatCardModule} from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
 import { CommandsComponent } from './components/commands/commands.component';
-import {MatTableModule} from "@angular/material/table";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatPaginatorModule} from "@angular/material/paginator";
-import {MatSortModule} from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
 import { ShowCommandComponent } from './components/commands/show-command/show-command.component';
-import {MatDialogModule} from "@angular/material/dialog";
+import { MatDialogModule } from "@angular/material/dialog";
 import { FeatureCardComponent } from './components/feature-card/feature-card.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        NavbarComponent,
         CommandsComponent,
         ShowCommandComponent,
         FeatureCardComponent,
@@ -38,20 +27,12 @@ import { FeatureCardComponent } from './components/feature-card/feature-card.com
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
-        FormsModule,
         BrowserAnimationsModule,
         MatButtonModule,
         MatToolbarModule,
-        MatSidenavModule,
-        MatListModule,
-        MatTabsModule,
-        MatOptionModule,
-        MatSelectModule,
         MatCardModule,
         MatTableModule,
-        MatFormFieldModule,
         MatPaginatorModule,
-        MatSortModule,
         MatDialogModule,
     ],
     providers: [],
